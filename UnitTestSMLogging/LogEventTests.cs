a3add67fa181304730bd9917170351d595897542
Mathias H. O <mathias_ottosen@hotmail.com>
2023-02-25 16:52:48 +0100

    Added LogEvent tests

---
+++ UnitTestSMLogging/LogEventTests.cs
@@ -0,0 +1,25 @@
+﻿using SMLogging;
+
+namespace UnitTestSMLogging
+{
+    public class LogEventTests
+    {
+        [InlineData(Level.Verbose)]
+        [InlineData(Level.Debug)]
+        [InlineData(Level.Information)]
+        [InlineData(Level.Warning)]
+        [InlineData(Level.Error)]
+        [InlineData(Level.Fatal)]
+        [Theory]
+        public void LogEvent_ConstructWithLevel_ShouldHaveLevel(Level level)
+        {
+            //Arrange
+            var logEvent = new LogEvent(level);
+
+            //Act
+
+            //Assert
+            Assert.Equal(level, logEvent.Level);
+        }
+    }
+}
