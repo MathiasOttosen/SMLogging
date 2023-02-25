904ee8f115ee58f2ed81a414291b649491872988
Mathias <Mathias@DESKTOP-6PKUO6P>
2023-02-25 15:26:29 +0100

    Add project files.

---
+++ SMLogging/Logger.cs
@@ -0,0 +1,21 @@
+﻿namespace SMLogging
+{
+    public class Logger
+    {
+        private Level minimumLevel;
+
+        public Logger(Level minimumLevel)
+        {
+            this.minimumLevel = minimumLevel;
+        }
+
+        public void Log(Level level, string message)
+        {
+            if (level >= minimumLevel)
+            {
+                // Log to selected out
+            }
+        }
+    }
+    public enum Level { Verbose, Debug, Information, Warning, Error, Fatal }
+}
