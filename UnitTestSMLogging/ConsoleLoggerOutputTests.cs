35ef4eca24214b4fd3ab640574ee10dbcb097b8c
Mathias H. O <mathias_ottosen@hotmail.com>
2023-02-25 18:59:59 +0100

    added test for writting to console

---
+++ UnitTestSMLogging/ConsoleLoggerOutputTests.cs
@@ -0,0 +1,26 @@
+﻿using SMLogging;
+
+namespace UnitTestSMLogging
+{
+    public class ConsoleLoggerOutputTests
+    {
+        [Fact]
+        public void LogToOutput_Called_ShouldWriteToConsole()
+        {
+            // Arrange
+            var loggerOutput = new ConsoleLoggerOutput();
+            var logEvent = new LogEvent();
+            var expected = logEvent.ToString();
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            // Act
+            loggerOutput.LogToOutput(logEvent);
+
+            // Assert
+            var output = stringWriter.ToString().Split(Environment.NewLine);
+            Assert.Equal(logEvent.ToString(), output[0]);
+
+        }
+    }
+}
